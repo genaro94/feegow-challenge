f328b6e3a439fbb3793eb81ded7b6c8ae8d67430
Genaro Rego Figueiredo <genarofigueiredo@hotmail.com>
2022-09-21 21:42:22 -0300

    test store schedule with message success

---
+++ tests/Feature/Schedule/ScheduleStoreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Schedule;
+
+use Tests\TestCase;
+use Carbon\Carbon;
+
+class ScheduleStoreTest extends TestCase
+{
+    public function test_store_schedule(): void
+    {
+        $this->post('/schedule/store', [
+            'specialty_id'     => 1,
+            'professional_id'  => 2,
+            'name'             => 'Maria da Conceição Silva',
+            'cpf'              => 1234567890,
+            'source_id'        => 2,
+            'birthdate'        => '1990-09-09',
+            'date_time'        => Carbon::today()->format('Y-m-d H:m:s'),
+        ])->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('schedules', [
+            'cpf'    => '1234567890',
+        ]);
+    }
+
+    public function test_store_schedule_with_message_success(): void
+    {
+        $response = $this->post('/schedule/store', [
+            'specialty_id'     => 1,
+            'professional_id'  => 2,
+            'name'             => 'Maria da Conceição Silva',
+            'cpf'              => 1234567890,
+            'source_id'        => 2,
+            'birthdate'        => '1990-09-09',
+            'date_time'        => Carbon::today()->format('Y-m-d H:m:s'),
+        ])
+        ->assertSessionHasNoErrors()
+        ->with(['success' => __('Agendado com sucesso!')]);
+    }
+}
